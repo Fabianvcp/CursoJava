066bfe97e8f2465b23410463799f4f2635eec556
Fabian <gallardofabianvcpz@gmail.com>
2021-05-22 15:20:30 -0300

    Curso de java

---
+++ src/HolaMundoJava.java
@@ -0,0 +1,8 @@
+//mi clase en java
+public class HolaMundoJava {
+    //vamos a escribir un metodo
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");
+    }
+
+}
