72a2711d6592ba92c9477869cea442254476e772
Fabian <gallardofabianvcpz@gmail.com>
2021-05-22 17:58:41 -0300

    curso variables java

--- src/HolaMundoJava.java
+++ src/HolaMundoJava.java
@@ -2,7 +2,24 @@
 public class HolaMundoJava {
     //vamos a escribir un metodo
     public static void main(String[] args){
-        System.out.println("Hola Mundo");
+        //Variable numerica
+        int MiVariableNumerica = 10;
+        System.out.println(MiVariableNumerica);
+        //modificar el valor de una variable
+        MiVariableNumerica = 5;
+        System.out.println(MiVariableNumerica);
+
+        //cadena de texto, no es primitivo
+        String MiVariableCadena = "Saludos";
+        System.out.println(MiVariableCadena);
+
+         MiVariableCadena = "Adios";
+        System.out.println(MiVariableCadena);
+
+        long MiVariableEntera2 = 10L;
+
+        System.out.println(MiVariableEntera2);
+
     }
 
 }
